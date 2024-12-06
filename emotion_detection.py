363684ee919e638b49e7808944d053f92987d3a4
abbas <abbas.tariq.bhutto@gmail.com>
2024-12-06 13:52:13 -0500

    update 2

--- emotion_detection.py
+++ emotion_detection.py
@@ -21,8 +21,35 @@ def emotion_detector(text_to_analyze):
     if response.status_code == 200:
         # Return the text from the response JSON
         print("done")
-        return response.json().get('text', 'No text found in the response')
-        print(response)
+        emotions = response.json()
+        emotions = emotions['emotionPredictions'][0]['emotion']
+
+        print(emotions)
+        
+                # Initialize emotion scores
+        anger_score = emotions.get('anger', 0)
+        disgust_score = emotions.get('disgust', 0)
+        fear_score = emotions.get('fear', 0)
+        joy_score = emotions.get('joy', 0)
+        sadness_score = emotions.get('sadness', 0)
+        
+        # Create a dictionary to store the emotions and their scores
+        emotion_scores = {
+            'anger': anger_score,
+            'disgust': disgust_score,
+            'fear': fear_score,
+            'joy': joy_score,
+            'sadness': sadness_score
+        }
+
+        dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+
+            # Add the dominant emotion to the result
+        emotion_scores['dominant_emotion'] = dominant_emotion
+        
+        return emotion_scores
+
+
     else:
         return f"Error: {response.status_code}"
 
@@ -30,6 +57,6 @@ def emotion_detector(text_to_analyze):
 
 # Example usage of the function
 if __name__ == "__main__":
-    text_to_analyze = "I love this new technology."
+    text_to_analyze = "I am glad this happened"
     result = emotion_detector(text_to_analyze)
     print(result)
