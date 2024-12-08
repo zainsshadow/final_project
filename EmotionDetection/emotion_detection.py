345f82a4c16c17d82480f1fe1e0c043b820c56eb
Your Name <you@example.com>
2024-12-08 08:42:44 -0500

    update

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -16,30 +16,21 @@ def emotion_detector(text_to_analyze):
     }
     # Send the POST request to the Watson NLP API
     response = requests.post(url, headers=headers, json=data)
-    
+
     # Check if the request was successful
     if response.status_code == 200:
         # Return the text from the response JSON
         print("done")
         emotions = response.json()
         emotions = emotions['emotionPredictions'][0]['emotion']
-
-        print(emotions)
-        
-                # Initialize emotion scores
-        anger_score = emotions.get('anger', 0)
-        disgust_score = emotions.get('disgust', 0)
-        fear_score = emotions.get('fear', 0)
-        joy_score = emotions.get('joy', 0)
-        sadness_score = emotions.get('sadness', 0)
         
         # Create a dictionary to store the emotions and their scores
         emotion_scores = {
-            'anger': anger_score,
-            'disgust': disgust_score,
-            'fear': fear_score,
-            'joy': joy_score,
-            'sadness': sadness_score
+            'anger': emotions.get('anger', 0),
+            'disgust': emotions.get('disgust', 0),
+            'fear': emotions.get('fear', 0),
+            'joy': emotions.get('joy', 0),
+            'sadness': emotions.get('sadness', 0)
         }
 
         dominant_emotion = max(emotion_scores, key=emotion_scores.get)
@@ -56,7 +47,7 @@ def emotion_detector(text_to_analyze):
 
 
 # Example usage of the function
-if __name__ == "__main__":
-    text_to_analyze = "I am glad this happened"
-    result = emotion_detector(text_to_analyze)
-    print(result)
+#if __name__ == "__main__":
+#   text_to_analyze = "I am glad this happened"
+#  result = emotion_detector(text_to_analyze)
+#  print(result)
